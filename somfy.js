499a556686085c5a029f72088747d3074e530c7b
Dennis Gilbert <dennis@pdx.edu>
2016-03-10 17:59:56 -0800

    Added channel selection code

--- somfy.js
+++ somfy.js
@@ -20,8 +20,14 @@ var ch2FlashCount = 0;
 // Somfy Channels
 
 const doorBlindChan = 2;
-const livingRoomBlindChan = 2;
+const livingRoomBlindChan = 3;
 const allBlindsChan = 4;
+
+// Time Durations
+
+const chanDisplayTimeout = 6000;
+const chanDetectPeriod = 1000;
+const chanPulseDuration = 100;
 
 var isChannel1 = function() {
 }
@@ -69,25 +75,73 @@ exports.livingRoomdoorLower = function() {
     lower();
 }
 
-function pulse(gpio, cb) {
+function pulse(gpio, next) {
     //Set gpio.gpioPin output, low;
     console.log("pulse(): setting gpio: " + gpio.gpioPin + " to low");
-    var err;
+    var err; // = "oops";
 
     //setTimeout(setHighZ(err), gpio.duration);
-    setTimeout(setHighZ(err), 10000);
+    setTimeout(setHighZ, gpio.duration, err, gpio);
 
-    function setHighZ(err) {
-	if (err) { return cb(err); }
+    function setHighZ(err, gpio) {
 	// Set gpio.gpioPin input;
+	if (err) { return next(err); }
+	
 	console.log("pulse(): setting gpio: " + gpio.gpioPin + " to high-Z");
-	setTimeout(cb, gpio.duration);
+	setTimeout(next, gpio.duration, err);
+    }
+}
+// desired = {blind: "door/familyroom/all", direction: "up/down"}
+function blindsActivate(desired, next) {
+    findChannelFive();
+
+    function selectChannel() {
+	if (desired.blind == "all") {
+	} else if (desired.blind == "door") {
+	} else {
+	}
+    }
+    function chanTwoActive(desired, err) {
+	pulse({gpioPin: channelpin, chanPulseDuration}, function activate() {
+	};)
+    }
+
+    function switchChannel(chanDiff, correctChannel) {
+	if (chanDiff == 0) { return correctChannel(); }
+	pulse({gpioPin: channelpin, chanPulseDuration}, function {
+	    switchChannel(chanDiff - 1, correctChannel);
+	});
     }
 }
 
+function findChanelFive(next) {
+    var chOneCnt = 0, chTwoCnt = 0;
+    // Set edge count callbacks
+
+    // Pulse channel select
+    pulse( {gpioPin: chanPin, duration: chanPulseDuration}, function detPause() {
+	setTimeout(detectCh5, chanDetectPeriod);
+    });
+    // and wait for flashes
+
+    // Check if in channel 5
+    function detectCh5(){
+	if (chOneCnt > 1 && chTwoCnt > 1) {
+	    next();
+	} else {
+	    findChannelFive(next);
+	}
+    }
+    // Pulse channel select
+}
+
 function test() {
-    pulse({gpioPin: 4, duration: 1000}, function() {
-	console.log("test(): finished pulsing");
+    pulse({gpioPin: 4, duration: 1000}, function(err) {
+	if (err) {
+	    console.log("test(): pulse produced error: " + err);
+	} else {
+	    console.log("test(): finished pulsing");
+	}
     });
 }
 
