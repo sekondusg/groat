a1f239ec46783c0b6f7eab17a4b1cb075ef58e95
Dennis Gilbert <dennis@pdx.edu>
2016-03-29 11:33:38 -0700

    Working: sunset and sun asimuth window tangent time calculated

--- events/config.py
+++ events/config.py
@@ -1,7 +1,12 @@
 class Config:
     def __init__(self):
+        # For the house latitude and longitude, the magnetic declination is 15.52 degrees East
+        # The normal vector from the sun facing side of the house is 17 degrees South of West.
+        # The solar azimuth for exposure is then: (270 - (17 - 15.52)) - 90 ~= 179 degrees (relative to East of North)
+        # For calculating magetic declination: http://www.ngdc.noaa.gov/geomag-web/#declination
+        # Used the returned URL from Google maps for determining latitude and longitude
         self.home = {
-            'solar_azimuth': 343,
+            'solar_azimuth': 179, # Units in degrees measured from South and going clockwise
             'lat': 45.5205163,
             'lon': -122.7482624
         }
--- events/solar_events.py
+++ events/solar_events.py
@@ -1,35 +1,47 @@
 from config import config
 import ephem
 import math
+from datetime import datetime
 
 class Solar:
     def __init__(self):
         self.obs = ephem.Observer()
         home = config.home; self.home = home
-        self.obs.lat = home['lat']
-        self.obs.lon = home['lon']
+        self.obs.lat = str(home['lat']) # ephem expects coordinates as strings
+        self.obs.lon = str(home['lon']) # ephem expects coordinates as strings
 
-    def time_of_solar_azimuth(self):
+    def time_of_solar_azimuth_today(self, azimuth):
         sun = ephem.Sun()
         sun.compute(self.obs)
-
+        self.obs.date = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
+        
         def err_az(x):
             self.obs.date = x
             sun.compute(self.obs)
             err_delta = sun.az - (math.pi/180. * self.home['solar_azimuth'])
-            #print('err: ', sun.az, ', solar_azimuth',  ephem.degrees(self.home['solar_azimuth']))
-            print('az: ', sun.az, ', solar_azimuth',  self.home['solar_azimuth'], ', delta: ', err_delta)
-            #return sun.az - ephem.degrees(self.home['solar_azimuth'])
+            #print('az: ', sun.az, ', solar_azimuth',  self.home['solar_azimuth'], ', delta: ', err_delta)
             return err_delta
 
-        self.obs.date = '2016/06/21 19:45:00'
-
         x = self.obs.date
-        print 'Searching for the correct time...'
+        #print 'Searching for the correct time...'
         ephem.newton(err_az, x, x + 0.01)
-        print 'At the time and date: ', self.obs.date
-        print 'the solar azimuth is', sun.az
+        return(self.obs.date)
+
+    def time_of_sunset_today(self):
+        #sun = ephem.Sun()
+        #sun.compute(self.obs)
+        obs = ephem.Observer()
+        home = config.home
+        obs.lat = str(home['lat'])
+        obs.lon = str(home['lon'])
+        obs.date = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
+
+        return obs.next_setting(ephem.Sun())
 
 if __name__ == '__main__':
     sol = Solar()
-    sol.time_of_solar_azimuth()
+    az_dt = sol.time_of_solar_azimuth_today(sol.home['solar_azimuth'])
+    print 'Azimuth reached today at: ', az_dt
+
+    sunset = sol.time_of_sunset_today()
+    print 'Sunset today at: ', sunset
